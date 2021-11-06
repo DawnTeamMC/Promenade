f6e95535ff36308046c9b17e0b85b3122d7f698f
Hugman <contact.hugman@gmail.com>
2021-11-06 23:58:16 +0100

    Slightly changed the colors of autumn birch leaves

--- src/main/java/com/hugman/promenade/init/client/PromenadeColorMaps.java
+++ src/main/java/com/hugman/promenade/init/client/PromenadeColorMaps.java
@@ -20,7 +20,7 @@ public class PromenadeColorMaps {
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> FoliageColors.getSpruceColor(), PromenadeBlocks.SPRUCE_LEAF_PILE);
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> FoliageColors.getBirchColor(), PromenadeBlocks.BIRCH_LEAF_PILE);
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15232304, PromenadeBlocks.AUTUMN_OAK_LEAVES, PromenadeBlocks.AUTUMN_OAK_LEAF_PILE);
-		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15645495, PromenadeBlocks.AUTUMN_BIRCH_LEAVES, PromenadeBlocks.AUTUMN_BIRCH_LEAF_PILE);
+		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 16759362, PromenadeBlocks.AUTUMN_BIRCH_LEAVES, PromenadeBlocks.AUTUMN_BIRCH_LEAF_PILE);
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15768259, PromenadeBlocks.PINK_CHERRY_OAK_LEAVES, PromenadeBlocks.PINK_CHERRY_OAK_LEAF_PILE);
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15786729, PromenadeBlocks.WHITE_CHERRY_OAK_LEAVES, PromenadeBlocks.WHITE_CHERRY_OAK_LEAF_PILE);
 	}
@@ -28,7 +28,7 @@ public class PromenadeColorMaps {
 	private static void registerItemColors() {
 		ColorProviderRegistry.ITEM.register((item, layer) -> GrassColors.getColor(0.5D, 1.0D), PromenadeBlocks.PALM_WOOD.getLeaves(), PromenadeBlocks.PALM_LEAF_PILE, PromenadeBlocks.OAK_LEAF_PILE, PromenadeBlocks.SPRUCE_LEAF_PILE, PromenadeBlocks.BIRCH_LEAF_PILE, PromenadeBlocks.JUNGLE_LEAF_PILE, PromenadeBlocks.ACACIA_LEAF_PILE, PromenadeBlocks.DARK_OAK_LEAF_PILE);
 		ColorProviderRegistry.ITEM.register((item, layer) -> 15232304, PromenadeBlocks.AUTUMN_OAK_LEAVES, PromenadeBlocks.AUTUMN_OAK_LEAF_PILE);
-		ColorProviderRegistry.ITEM.register((item, layer) -> 15645495, PromenadeBlocks.AUTUMN_BIRCH_LEAVES, PromenadeBlocks.AUTUMN_BIRCH_LEAF_PILE);
+		ColorProviderRegistry.ITEM.register((item, layer) -> 16759362, PromenadeBlocks.AUTUMN_BIRCH_LEAVES, PromenadeBlocks.AUTUMN_BIRCH_LEAF_PILE);
 		ColorProviderRegistry.ITEM.register((item, layer) -> 15768259, PromenadeBlocks.PINK_CHERRY_OAK_LEAVES, PromenadeBlocks.PINK_CHERRY_OAK_LEAF_PILE);
 		ColorProviderRegistry.ITEM.register((item, layer) -> 15786729, PromenadeBlocks.WHITE_CHERRY_OAK_LEAVES, PromenadeBlocks.WHITE_CHERRY_OAK_LEAF_PILE);
 	}
