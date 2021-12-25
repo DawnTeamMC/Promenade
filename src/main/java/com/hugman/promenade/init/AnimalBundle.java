21f6cd1a1edf0695f3e394dd790a1643f4d8cd89
Hugman <contact.hugman@gmail.com>
2021-12-25 17:44:41 +0100

    Restricted duck spawning

--- src/main/java/com/hugman/promenade/init/AnimalBundle.java
+++ src/main/java/com/hugman/promenade/init/AnimalBundle.java
@@ -12,14 +12,25 @@ import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.SpawnEggItem;
+import net.minecraft.world.Heightmap;
 
 import java.util.function.Predicate;
 
 public class AnimalBundle extends PromenadeBundle {
-	public static final EntityType<DuckEntity> DUCK = add(new EntityCreator<>("duck", FabricEntityTypeBuilder.createLiving().entityFactory(DuckEntity::new).spawnGroup(SpawnGroup.CREATURE).dimensions(EntityDimensions.fixed(0.4F, 0.8F)).trackRangeChunks(10).trackedUpdateRate(3).defaultAttributes(DuckEntity::createDuckAttributes).build()));
+	public static final EntityType<DuckEntity> DUCK = add(new EntityCreator<>("duck", FabricEntityTypeBuilder.createMob()
+			.entityFactory(DuckEntity::new)
+			.spawnGroup(SpawnGroup.CREATURE)
+			.dimensions(EntityDimensions.fixed(0.4F, 0.8F))
+			.trackRangeChunks(10)
+			.trackedUpdateRate(3)
+			.defaultAttributes(DuckEntity::createDuckAttributes)
+			.spawnRestriction(SpawnRestriction.Location.ON_GROUND, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, AnimalEntity::isValidNaturalSpawn)
+			.build()));
 	public static final Item DUCK_SPAWN_EGG = add(new ItemCreator.Builder("duck_spawn_egg", settings -> new SpawnEggItem(AnimalBundle.DUCK, 10592673, 15904341, settings), new Item.Settings().group(ItemGroup.MISC)).build());
 	public static final SoundCreator DUCK_AMBIENT_SOUND = creator(new SoundCreator("entity.duck.ambient"));
 	public static final SoundCreator DUCK_HURT_SOUND = creator(new SoundCreator("entity.duck.hurt"));
