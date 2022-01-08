33c6e3e65eab2008521ce113f03b3895db011134
Hugman <contact.hugman@gmail.com>
2022-01-08 08:05:58 +0100

    Updated cherry textures

--- src/main/java/com/hugman/promenade/init/client/PromenadeColorMaps.java
+++ src/main/java/com/hugman/promenade/init/client/PromenadeColorMaps.java
@@ -24,15 +24,11 @@ public class PromenadeColorMaps {
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> FoliageColors.getBirchColor(), VanillaPilesBundle.BIRCH_LEAF_PILE);
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15232304, AutumnBundle.AUTUMN_OAK_LEAVES, AutumnBundle.AUTUMN_OAK_LEAF_PILE);
 		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 16759362, AutumnBundle.AUTUMN_BIRCH_LEAVES, AutumnBundle.AUTUMN_BIRCH_LEAF_PILE);
-		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15768259, CherryBundle.PINK_CHERRY_OAK_LEAVES, CherryBundle.PINK_CHERRY_OAK_LEAF_PILE);
-		ColorProviderRegistry.BLOCK.register((block, pos, world, layer) -> 15786729, CherryBundle.WHITE_CHERRY_OAK_LEAVES, CherryBundle.WHITE_CHERRY_OAK_LEAF_PILE);
 	}
 
 	private static void registerItemColors() {
 		ColorProviderRegistry.ITEM.register((item, layer) -> GrassColors.getColor(0.5D, 1.0D), PalmBundle.PALM_WOOD.getLeaves(), PalmBundle.PALM_LEAF_PILE, VanillaPilesBundle.OAK_LEAF_PILE, VanillaPilesBundle.SPRUCE_LEAF_PILE, VanillaPilesBundle.BIRCH_LEAF_PILE, VanillaPilesBundle.JUNGLE_LEAF_PILE, VanillaPilesBundle.ACACIA_LEAF_PILE, VanillaPilesBundle.DARK_OAK_LEAF_PILE);
 		ColorProviderRegistry.ITEM.register((item, layer) -> 15232304, AutumnBundle.AUTUMN_OAK_LEAVES, AutumnBundle.AUTUMN_OAK_LEAF_PILE);
 		ColorProviderRegistry.ITEM.register((item, layer) -> 16759362, AutumnBundle.AUTUMN_BIRCH_LEAVES, AutumnBundle.AUTUMN_BIRCH_LEAF_PILE);
-		ColorProviderRegistry.ITEM.register((item, layer) -> 15768259, CherryBundle.PINK_CHERRY_OAK_LEAVES, CherryBundle.PINK_CHERRY_OAK_LEAF_PILE);
-		ColorProviderRegistry.ITEM.register((item, layer) -> 15786729, CherryBundle.WHITE_CHERRY_OAK_LEAVES, CherryBundle.WHITE_CHERRY_OAK_LEAF_PILE);
 	}
 }
