25385e4bec17b5487e867101f864ca27aaf0c782
Hugman <contact.hugman@gmail.com>
2022-10-31 21:08:33 +0100

    Fixed an issue with palm wood

--- src/main/java/com/hugman/promenade/init/PalmBundle.java
+++ src/main/java/com/hugman/promenade/init/PalmBundle.java
@@ -17,9 +17,8 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.PlacedFeature;
 
-public class PalmBundle extends PromenadeBundle
-{
-	public static final OverworldWoodBundle PALM_WOOD = creator(new OverworldWoodBundle.Builder("palm", new SingleSaplingGenerator(), MapColor.ORANGE, MapColor.TERRACOTTA_CYAN).saplingSoil(blockState -> blockState.isIn(BlockTags.SAND)).build());
+public class PalmBundle extends PromenadeBundle {
+	public static final OverworldWoodBundle PALM_WOOD = creator(new OverworldWoodBundle.Builder("palm", new SingleSaplingGenerator("palm"), MapColor.ORANGE, MapColor.TERRACOTTA_CYAN).saplingSoil(blockState -> blockState.isIn(BlockTags.SAND)).build());
 	public static final Block PALM_LEAF_PILE = add(PromenadeBlockBuilders.leafPile("palm_leaf_pile").build());
 
 	public static final RegistryKey<PlacedFeature> PALMS = WorldGenUtil.placedFeatureKey("trees/palms");
