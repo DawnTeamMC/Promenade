ccf386cc3dbfb06f309c5250be0ef788c6cdd5e7
Hugman <contact.hugman@gmail.com>
2022-12-23 15:10:54 +0100

    Move tag factories to Dawn

--- src/main/java/fr/hugman/promenade/registry/tag/PromenadeBiomeTags.java
+++ src/main/java/fr/hugman/promenade/registry/tag/PromenadeBiomeTags.java
@@ -1,13 +1,14 @@
 package fr.hugman.promenade.registry.tag;
 
-import fr.hugman.promenade.PromenadeFactory;
+import fr.hugman.dawn.DawnFactory;
+import fr.hugman.promenade.Promenade;
 import net.minecraft.registry.tag.TagKey;
 import net.minecraft.world.biome.Biome;
 
 public class PromenadeBiomeTags {
-	public static final TagKey<Biome> CAPYBARA_SPAWN = PromenadeFactory.biomeTag("spawns/capybara");
-	public static final TagKey<Biome> PEKIN_DUCK_SPAWN = PromenadeFactory.biomeTag("spawns/duck/pekin");
-	public static final TagKey<Biome> MALLARD_DUCK_SPAWN = PromenadeFactory.biomeTag("spawns/duck/mallard");
-	public static final TagKey<Biome> SUNKEN_SKELETON_SPAWN = PromenadeFactory.biomeTag("spawns/sunken_skeleton");
-	public static final TagKey<Biome> HAS_PALMS = PromenadeFactory.biomeTag("has_palms");
+	public static final TagKey<Biome> CAPYBARA_SPAWN = DawnFactory.biomeTag(Promenade.id("spawns/capybara"));
+	public static final TagKey<Biome> PEKIN_DUCK_SPAWN = DawnFactory.biomeTag(Promenade.id("spawns/duck/pekin"));
+	public static final TagKey<Biome> MALLARD_DUCK_SPAWN = DawnFactory.biomeTag(Promenade.id("spawns/duck/mallard"));
+	public static final TagKey<Biome> SUNKEN_SKELETON_SPAWN = DawnFactory.biomeTag(Promenade.id("spawns/sunken_skeleton"));
+	public static final TagKey<Biome> HAS_PALMS = DawnFactory.biomeTag(Promenade.id("has_palms"));
 }
--- src/main/java/fr/hugman/promenade/registry/tag/PromenadeItemTags.java
+++ src/main/java/fr/hugman/promenade/registry/tag/PromenadeItemTags.java
@@ -1,10 +1,11 @@
 package fr.hugman.promenade.registry.tag;
 
-import fr.hugman.promenade.PromenadeFactory;
+import fr.hugman.dawn.DawnFactory;
+import fr.hugman.promenade.Promenade;
 import net.minecraft.item.Item;
 import net.minecraft.registry.tag.TagKey;
 
 public class PromenadeItemTags {
-	public static final TagKey<Item> BREEDING_CAPYBARA = PromenadeFactory.itemTag("breeding/capybara");
-	public static final TagKey<Item> BREEDING_DUCK = PromenadeFactory.itemTag("breeding/duck");
+	public static final TagKey<Item> BREEDING_CAPYBARA = DawnFactory.itemTag(Promenade.id("breeding/capybara"));
+	public static final TagKey<Item> BREEDING_DUCK = DawnFactory.itemTag(Promenade.id("breeding/duck"));
 }
