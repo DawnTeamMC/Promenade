48a9cf9c1cc18f51ea861cdb39206fb173b10676
Hugman <contact.hugman@gmail.com>
2023-11-14 00:03:56 +0100

    Try a better approach for cypress foliage

--- src/main/java/fr/hugman/promenade/gen/tree/foliage/CypressFoliagePlacer.java
+++ src/main/java/fr/hugman/promenade/gen/tree/foliage/CypressFoliagePlacer.java
@@ -7,6 +7,8 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.math.intprovider.IntProvider;
+import net.minecraft.util.math.intprovider.IntProviderType;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.TestableWorld;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
@@ -35,16 +37,21 @@ public class CypressFoliagePlacer extends FoliagePlacer {
     protected void generate(TestableWorld world, BlockPlacer placer, Random random, TreeFeatureConfig config, int trunkHeight, TreeNode treeNode, int foliageHeight, int radius, int offset) {
         var pos = treeNode.getCenter().down(trunkHeight - offset);
 
-        var h = foliageHeight;
-        var b = radius / 2;
-        for (int y = 0; y < foliageHeight; y++) {
-            var a = 10.0f * y / h;
-            float r = (float) ((-0.003 * Math.pow(a, 4) +
-                    0.0725 * Math.pow(a, 3) +
-                    -0.64 * Math.pow(a, 2) +
-                    2.2 * Math.pow(a, 1)
-                    - 0.555) * b);
-            this.generateCircle(world, placer, random, config, pos, r, y, treeNode.isGiantTrunk());
+        //TODO: support giant trunks
+        //TODO: more configurability?
+        for (int dx = -radius; dx <= radius; dx++) {
+            for (int dz = -radius; dz <= radius; dz++) {
+
+                int d = Math.abs(dx) + Math.abs(dz);
+
+                // the further we are from the trunk, the lower the height
+                int y1 = Math.max(0, d - 1 + offset);
+                int y2 = Math.min(foliageHeight, foliageHeight - d * 3); //TODO: radius higher => higher height malus (*1 - *1.5 - *2 - *2.5 - *3...)
+
+                if(y2 <= y1) continue;
+
+                this.generateColumn(world, placer, config, random, pos, dz, dx, y1, y2);
+            }
         }
     }
 
@@ -55,22 +62,18 @@ public class CypressFoliagePlacer extends FoliagePlacer {
 
     @Override
     protected boolean isInvalidForLeaves(Random random, int dx, int y, int dz, int radius, boolean giantTrunk) {
+        if (dx == 0 && dz == 0) return true;
+        var d = dx + dz;
+        if (d > radius) return true;
         return false;
     }
 
-    protected void generateCircle(TestableWorld world, BlockPlacer placer, Random random, TreeFeatureConfig config, BlockPos centerPos, float radius, int y, boolean giantTrunk) {
-        int i = giantTrunk ? 1 : 0;
+    protected void generateColumn(TestableWorld world, BlockPlacer placer, TreeFeatureConfig config, Random random, BlockPos centerPos, int dx, int dz, int y1, int y2) {
         BlockPos.Mutable mutable = new BlockPos.Mutable();
-        float radiusRemainder = radius % 1.0F;
-        int radiusInt = (int) (radius) + 1;
-        for (int x = -radiusInt; x <= radiusInt + i; ++x) {
-            for (int z = -radiusInt; z <= radiusInt + i; ++z) {
-                var r = MathHelper.abs(x) + MathHelper.abs(z);
-                if (r > radiusInt) continue;
-                if (r == radiusInt && 0.5 < radiusRemainder) continue;
-                mutable.set(centerPos, x, y, z);
-                FoliagePlacer.placeFoliageBlock(world, placer, random, config, mutable);
-            }
+        mutable.set(centerPos, dz, y1, dx);
+        for (int y = y1; y < y2; y++) {
+            mutable.move(0, 1, 0);
+            FoliagePlacer.placeFoliageBlock(world, placer, random, config, mutable);
         }
     }
 }
