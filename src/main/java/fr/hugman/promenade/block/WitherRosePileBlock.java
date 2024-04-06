393cebad5c30c630aa30b543b62c92af7e3ec051
Hugo Landrin <contact.hugman@gmail.com>
2024-04-06 17:13:28 +0200

    Wtf git?

--- src/main/java/fr/hugman/promenade/block/WitherRosePileBlock.java
+++ src/main/java/fr/hugman/promenade/block/WitherRosePileBlock.java
@@ -5,7 +5,6 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.PlantBlock;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
