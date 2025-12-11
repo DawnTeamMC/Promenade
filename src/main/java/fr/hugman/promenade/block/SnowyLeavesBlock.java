30031d92ca0c8affbc1eaa6c67af6a851796c4c2
Hugman <contact.hugman@gmail.com>
2025-12-11 22:44:58 +0100

    Snowy Leaves now melt under light

--- src/main/java/fr/hugman/promenade/block/SnowyLeavesBlock.java
+++ src/main/java/fr/hugman/promenade/block/SnowyLeavesBlock.java
@@ -1,15 +1,20 @@
 package fr.hugman.promenade.block;
 
+import fr.hugman.promenade.registry.PromenadeRegistryKeys;
+import fr.hugman.promenade.tag.PromenadeBiomeTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
+import net.minecraft.world.LightType;
 import net.minecraft.world.WorldView;
 import net.minecraft.world.tick.ScheduledTickView;
 
@@ -33,6 +38,29 @@ public abstract class SnowyLeavesBlock extends LeavesBlock {
         if (state == null) return null;
         BlockState stateBelow = context.getWorld().getBlockState(context.getBlockPos().down());
         return state.with(BOTTOM, !isSnow(stateBelow));
+    }
+
+    @Override
+    protected boolean hasRandomTicks(BlockState state) {
+        return true;
+    }
+
+    @Override
+    protected void randomTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+        super.randomTick(state, world, pos, random);
+
+        if (world.getLightLevel(LightType.BLOCK, pos) > 11 && !world.getBiome(pos).isIn(PromenadeBiomeTags.CAN_FREEZE_DURING_SNOWFALL)) {
+            var normalLeaves = world.getRegistryManager().getOrThrow(PromenadeRegistryKeys.SNOWY_BLOCK_TRANSFORMATION).stream().filter(
+                    entry -> entry.snowyBlock().value() == state.getBlock()
+            ).findFirst().map(sbt -> sbt.baseBlock().value()).orElse(null);
+
+            var newLeavesState = normalLeaves.getDefaultState();
+            // copy properties of snowy block
+            for (Property property : state.getProperties()) {
+                newLeavesState = newLeavesState.contains(property) ? newLeavesState.with(property, state.get(property)) : newLeavesState;
+            }
+            world.setBlockState(pos, newLeavesState);
+        }
     }
 
     @Override
