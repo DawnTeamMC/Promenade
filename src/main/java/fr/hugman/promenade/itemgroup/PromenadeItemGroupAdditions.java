74d02efd82e97bf65a445a156a193b6e2147f10b
Hugman <contact.hugman@gmail.com>
2026-01-10 18:11:55 +0100

    Re-organize Promenade spawn eggs in creative

--- src/main/java/fr/hugman/promenade/itemgroup/PromenadeItemGroupAdditions.java
+++ src/main/java/fr/hugman/promenade/itemgroup/PromenadeItemGroupAdditions.java
@@ -5,10 +5,6 @@ import fr.hugman.promenade.item.PromenadeItems;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.*;
-import net.minecraft.registry.Registries;
-
-import java.util.Collections;
-import java.util.function.Predicate;
 
 public class PromenadeItemGroupAdditions {
     public static void appendItemGroups() {
@@ -209,33 +205,9 @@ public class PromenadeItemGroupAdditions {
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(e -> e.addAfter(Items.FLOWER_BANNER_PATTERN, PromenadeItems.BOVINE_BANNER_PATTERN));
 
-        appendSpawnEgg(PromenadeItems.CAPYBARA_SPAWN_EGG);
-        appendSpawnEgg(PromenadeItems.DUCK_SPAWN_EGG);
-        appendSpawnEgg(PromenadeItems.LUSH_CREEPER_SPAWN_EGG);
-        appendSpawnEgg(PromenadeItems.SUNKEN_SPAWN_EGG);
-    }
-
-    public static void appendSpawnEgg(Item spawnEgg) {
-        var itemGroup = Registries.ITEM_GROUP.get(ItemGroups.SPAWN_EGGS);
-        String path = Registries.ITEM.getId(spawnEgg).getPath();
-
-        if (itemGroup == null) {
-            return;
-        }
-
-        Predicate<ItemStack> predicate = stack1 -> {
-            String path1 = Registries.ITEM.getId(stack1.getItem()).getPath();
-            for (ItemStack stack2 : itemGroup.getDisplayStacks()) {
-                String path2 = Registries.ITEM.getId(stack2.getItem()).getPath();
-                if (path1.matches(".*_spawn_egg") && path2.matches(".*_spawn_egg")) {
-                    // check if path is lexicographically between path1 and path2
-                    if (path.compareTo(path1) > 0 && path.compareTo(path2) < 0) {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        };
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(e -> e.addAfter(predicate, Collections.singleton(new ItemStack(spawnEgg)), ItemGroup.StackVisibility.PARENT_AND_SEARCH_TABS));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(e -> e.addAfter(Items.CHICKEN_SPAWN_EGG, PromenadeItems.DUCK_SPAWN_EGG));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(e -> e.addAfter(Items.PIG_SPAWN_EGG, PromenadeItems.CAPYBARA_SPAWN_EGG));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(e -> e.addAfter(Items.CREEPER_SPAWN_EGG, PromenadeItems.LUSH_CREEPER_SPAWN_EGG));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(e -> e.addAfter(Items.DROWNED_SPAWN_EGG, PromenadeItems.SUNKEN_SPAWN_EGG));
     }
 }
